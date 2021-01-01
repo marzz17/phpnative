58fdaebdb3fd065e418651ab5bf7644ea71f5b6c
byruddy <rudihikmatullah@outlook.com>
2021-01-01 20:08:23 +0700

    Adding new function (GenerateRandomString)

--- config/helper.php
+++ config/helper.php
@@ -138,3 +138,15 @@
         }
     }
 
+
+    // Generate string random by Stephen Watkins (Stackoverflow)
+    function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
